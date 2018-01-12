447fa51bfc7730177c0fb2482e6820d084cae8bb
jayceelock <jaycee.lock@gmail.com>
2018-01-12 10:39:06 +0000

    Committing

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'dgwvdiyznr$dsc^=h4q15+u83^v=y!x$i6js!%h69k)w)2+xf6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['jclock.co.uk', ]
+ALLOWED_HOSTS = ['jclock.co.uk',
+                 '127.0.0.1', ]
 
 
 # Application definition
