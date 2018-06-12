69363b3d1c5029cb19c0ee98192e6f60e6d54305
Jaycee Lock <jaycee.lock@gmail.com>
2018-06-12 15:00:35 +0100

    Refactored template code to accommodate blog

--- BlogWebpage/urls.py
+++ BlogWebpage/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-        url(r'^', views.home, name="home"),
+        url(r'^', views.blog_posts, name="blog_posts"),
         ]
